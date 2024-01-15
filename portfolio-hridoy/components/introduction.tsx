fbc32cc3bba79759ef14031fbc6b2013ab0b9519
Hridoy <19202103103@cse.bubt.edu.bd>
2024-01-15 11:57:53 +0600

    CV update

--- portfolio-hridoy/components/introduction.tsx
+++ portfolio-hridoy/components/introduction.tsx
@@ -38,7 +38,7 @@ export default function Introduction() {
 
         <a
           className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-gray/10"
-          href="/Hridoy_s_CV.pdf"
+          href="/Hridoy.pdf"
           download
         >
           Download CV{" "}
